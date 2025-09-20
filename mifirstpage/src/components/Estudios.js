6f013db4e72a592fd7899b69a98fd4d4c4ec0dbd
FranciscoDJVI <vanegasfrancisco415@gmail.com>
2025-09-20 10:05:07 -0500

    font size

--- mifirstpage/src/components/Estudios.js
+++ mifirstpage/src/components/Estudios.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Estudies() {
   return (
-    <div className="studies" style={{ fontSize: "2rem" }}>
+    <div className="studies" style={{ fontSize: "3rem" }}>
       <h2>Estudios</h2>
       <ol className="estudies">
         <li className="estudies-li">Ingeniero de sistemas</li>
