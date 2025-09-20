90315e64744dfb0b416b4575141f30769658cbfe
FranciscoDJVI <vanegasfrancisco415@gmail.com>
2025-09-20 11:15:21 -0500

    insights

--- mifirstpage/src/index.js
+++ mifirstpage/src/index.js
@@ -1,11 +1,11 @@
-import { injectSpeedInsights } from "@vercel/speed-insights";
-injectSpeedInsights();
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { injectSpeedInsights } from "@vercel/speed-insights";
+
+injectSpeedInsights();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
